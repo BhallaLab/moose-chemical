aa4272d97a3f88e57e907371f1035109acf4782f
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-04 19:20:26 +0530

    both tests are passing.

--- test.py
+++ test.py
@@ -37,14 +37,14 @@ class TestGvChem( unittest.TestCase ):
         moose.delete('/synapse')
         tables = yacml.main(args)
         a, b, c = tables['a'], tables['b'], tables['c']
-        real, computed= 0.719224, c.vector[-1]
+        real, computed= 1.12311, c.vector[-1]
         error = abs((real - computed) / real)
         mu.plotRecords( 
                 { 'a' : a, 'b' : b, 'c' : c }
                 , outfile = 'simple_test_2c.png'
                 )
         steady_state = c.vector[-1]**2 / (a.vector[-1] * b.vector[-1])
-        #print("++ Solution: %s, computed: %s, error: %s" % (real, computed, error))
+        print("++ Solution: %s, computed: %s, error: %s" % (real, computed, error))
         print("|| Steady state: %s" % steady_state)
         self.assertTrue(error < 0.0001)
 
