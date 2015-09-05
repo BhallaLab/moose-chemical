e61b5dba72fd41bdf991a3b195a0ddd7d7da8a32
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-05 09:20:43 +0530

    Added 3 test.

--- test.py
+++ test.py
@@ -48,6 +48,23 @@ class TestGvChem( unittest.TestCase ):
         print("|| Steady state: %s" % steady_state)
         self.assertTrue(error < 0.0001)
 
+    def test_simple_expr(self):
+        global args
+        args['model_file'] = '_models/simple_expr.yml'
+        moose.delete('/synapse')
+        tables = yacml.main(args)
+        a, b, c = tables['a'], tables['b'], tables['c']
+        real, computed= 1.12311, c.vector[-1]
+        error = abs((real - computed) / real)
+        mu.plotRecords( 
+                { 'a' : a, 'b' : b, 'c' : c }
+                , outfile = 'simple_test_expr.png'
+                )
+        steady_state = c.vector[-1]**2 / (a.vector[-1] * b.vector[-1])
+        print("++ Solution: %s, computed: %s, error: %s" % (real, computed, error))
+        print("|| Steady state: %s" % steady_state)
+        self.assertTrue(error < 0.0001)
+
 
 
 if __name__ == '__main__':
