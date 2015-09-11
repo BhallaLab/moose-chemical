326fc30aa5014419303a21e9d474385de11d2df3
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-11 14:27:16 +0530

    Refactoring.

--- yacml.py
+++ yacml.py
@@ -14,22 +14,22 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 
-from parser import parser_yml
-from backend import chem_moose
-
+from yacml import chemviz
 import config
 import logging
 
 logger_ = logging.getLogger('yacml')
+logger_.setLevel(logging.DEBUG)
 
 def main(args):
+    """Main entry function
+    """
     config.args_ = args
     modelFile = args['model_file']
-
-    model = parser_yml.parse(modelFile)
     if args['solver'] == 'moose':
-        chem_moose.to_moose(model)
-        return chem_moose.run(args)
+        model = chemviz.DotModel(modelFile)
+        model.run(args)
+        return model
     elif args['solver'] == "scipy":
         logger_.error("Solver scipy is still not supported")
     else:
@@ -41,38 +41,32 @@ if __name__ == '__main__':
     # Argument parser.
     description = '''YACML: Yet Another Chemical Markup Language'''
     argp = argparse.ArgumentParser(description=description)
-    argp.add_argument('--sim_time', '-st'
-            , metavar='variable'
-            , required = True
-            , type = float
-            , help = 'A generic option'
-            )
     argp.add_argument('--model_file', '-f'
             , required = True
             , type = str
             , help = 'Model file'
             )
+    
+    argp.add_argument('--sim_time', '-st'
+            , metavar='variable'
+            , default = 10.0
+            , type = float
+            , help = 'A generic option'
+            )
+
     argp.add_argument('--solver', '-s'
-            , required = True
             , default = 'moose'
             , type = str
             , help = "Which solver to use: moose | scipy"
             )
 
-    argp.add_argument('--plot', '-p'
-            , action = 'store_true'
-            , help = "Plot the results?"
-            )
-
     argp.add_argument('--outfile', '-o'
-            , required = False
             , default = None
             , type = str
             , help = "Name of the plot file"
             )
 
     argp.add_argument('--log', '-l'
-            , required = False
             , default = 'warning'
             , type = str
             , help = 'Debug levels: [debug, info, warning, error, critical]'
